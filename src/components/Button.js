dc96cb17feae8faa7923b19b6f6fc44f479dd7e0
YoussefBenAmmar <benammar.y@gmail.com>
2023-02-21 12:49:59 -0500

    create DayListItem ecosystem

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,6 +3,14 @@ import classNames from "classnames";
 
 import "components/Button.scss";
 
+/** PROPS:
+ * Base uses no props and is considered the default button
+ * Confirm uses the confirm prop to apply the .button--confirm modifier class
+ * Danger uses the danger prop to apply the .button--danger modifier class
+ * Clickable uses the onClick prop to handle the button click event
+ * Disabled uses the disabled prop to apply the disabled attribute to the button element
+ */
+
 export default function Button(props) {
   const buttonClass = classNames("button", {
     "button--confirm": props.confirm,
---
+++ src/components/DayListItem.js
@@ -0,0 +1,39 @@
+import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss";
+
+/** PROPS 
+ * 
+ * name:String the name of the day
+ * spots:Number the number of spots remaining
+ * selected:Boolean true or false declaring that this day is selected
+ * setDay:Function accepts the name of the day eg. "Monday", "Tuesday"
+ * 
+ */
+
+export default function DayListItem(props) {
+
+  const formatSpots = () => {
+    if (props.spots === 0) {
+      return 'no spots remaining';
+    }
+
+    if (props.spots === 1) {
+      return `${props.spots} spot remaining`;
+    }
+    return `${props.spots} spots remaining`;
+  };
+
+  let dayClass = classNames("day-list__item", {
+    "day-list__item--selected ": props.selected,
+    "day-list__item--full": !props.spots,
+  });
+
+  return (
+    <li onClick={() => props.setDay(props.name)}
+      className={dayClass}>
+      <h2 >{props.name}</h2>
+      <h3 >{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
