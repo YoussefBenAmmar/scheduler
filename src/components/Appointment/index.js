99a828bb2ee147452508f0dd907c7eb73ec0eaec
YoussefBenAmmar <benammar.y@gmail.com>
2023-02-22 21:59:35 -0500

    scheduler initial set up

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,17 +4,14 @@ import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Status from "./Status";
 
 export default function Appointment(props){
 
   return(
     <article className="appointment">
       <Header time={props.time}/>
-      if ({props.interview}){
-        <Show/>
-      } else {
-        <Empty/>
-      }
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} />:<Empty/>  }
       </article>
     
   );
