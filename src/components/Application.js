37c8ceae8aade162308551ba8ded0ac59fddcf7e
YoussefBenAmmar <benammar.y@gmail.com>
2023-02-27 17:45:29 -0500

    delete&edit apointement && separation of concerns

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,56 +5,109 @@ import DayList from "./DayList";
 import DayListItem from "./DayListItem";
 import Appointment from "./Appointment";
 import axios from "axios";
-import { getAppointmentsForDay, getInterview } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
 import useVisualMode from "hooks/useVisualMode";
+import useApplicationData from "hooks/useApplicationData";
+
+
 
 
 
 
 export default function Application(props) {
 
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  } = useApplicationData();
+  
+  // const [state, setState] = useState({
+  //   day: "Monday",
+  //   days: [],
+  //   appointments: {},
+  //   interviewers: {},
+  // });
 
- 
-  const setDay = day => setState({ ...state, day });
 
+  // const setDay = day => setState({ ...state, day });
 
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {},
-  });
+  // function bookInterview(id, interview) {
+  //   return axios.put(`/api/appointments/${id}`, { interview }).then(() => {
 
-  useEffect(() => {
-    Promise.all([
-      axios.get(`http://localhost:8001/api/days`),
-      axios.get(`http://localhost:8001/api/appointments`),
-      axios.get(`http://localhost:8001/api/interviewers`),
-    ]).then((all) => {
-      setState(prev => ({
-        ...prev,
-        days: all[0].data,
-        appointments: all[1].data,
-        interviewers: all[2].data,
-      }));
-      console.log(all);
-    });
-  }, []);
+  //     console.log(id, interview);
+
+  //     const appointment = {
+  //       ...state.appointments[id],
+  //       interview: { ...interview }
+  //     };
+
+  //     const appointments = {
+  //       ...state.appointments,
+  //       [id]: appointment
+  //     };
+
+  //     setState({
+  //       ...state,
+  //       appointments
+  //     });
+  //   }
+  //   );
+  // }
+
+  // function cancelInterview(id) {
+
+  //   return axios.delete(`/api/appointments/${id}`).then(() => {
+
+  //   const appointment = {
+  //     ...state.appointments[id],
+  //     interview: null,
+  //   };
+  //   const appointments = {
+  //     ...state.appointments,
+  //     [id]: appointment,
+  //   };
+
+  //     setState({
+  //       ...state,
+  //       appointments
+  //     });
+  //   });    
+  // }
+
+  // useEffect(() => {
+  //   Promise.all([
+  //     axios.get(`http://localhost:8001/api/days`),
+  //     axios.get(`http://localhost:8001/api/appointments`),
+  //     axios.get(`http://localhost:8001/api/interviewers`),
+  //   ]).then((all) => {
+  //     setState(prev => ({
+  //       ...prev,
+  //       days: all[0].data,
+  //       appointments: all[1].data,
+  //       interviewers: all[2].data,
+  //     }));
+  //     console.log(all);
+  //   });
+  // }, []);
 
   const dailyAppointements = getAppointmentsForDay(state, state.day);
 
   const schedule = dailyAppointements.map((appointment) => {
     const interview = getInterview(state, appointment.interview);
-
-
+    const interviewers = getInterviewersForDay(state, state.day);
 
     return (
-    <Appointment
-      key={appointment.id}
-      id={appointment.id}
-      time={appointment.time}
-      interview={interview}
-    />
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+        interviewers={interviewers}
+        bookInterview={bookInterview}
+        cancelInterview={cancelInterview}
+      />
     );
   });
 
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,16 +6,46 @@ import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import Status from "./Status";
+import Confirm from "./Confirm";
 import Form from "./Form";
+import Application from "components/Application";
+import Error from "./Error";
 
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
 const FORM = "FORM";
+const SAVING = "SAVING";
+const DELETING = "DELETING";
+const CONFIRM = "CONFIRM";
+const EDIT = "EDIT";
+const ERROR_SAVE = "ERROR_SAVE";
+const ERROR_DELETE = "ERROR_DELETE";
 
 export default function Appointment(props) {
 
+
+  function save(name, interviewer){
+    transition(SAVING);
+    const interview = {
+      student: name,
+      interviewer
+    };
+    props.bookInterview(props.id, interview)
+    .then(() => {transition(SHOW);})
+    .catch(() => transition(ERROR_SAVE, true));
+  }
+
+  function deleting(){
+    transition(DELETING, true)
+
+    props.cancelInterview(props.id)
+      .then(() => {
+        transition(EMPTY);
+      })
+      .catch(() => { transition(ERROR_DELETE, true);});  
+    }
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
@@ -36,12 +66,43 @@ export default function Appointment(props) {
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
+          onDelete={() => transition(CONFIRM)}
+          onEdit={() => transition(EDIT)}
         />
       )}
-      {mode === CREATE && <Form interviewers={[]} 
+      {mode === CREATE && <Form interviewers={props.interviewers} 
       onCancel={onCancel}
+      onSave={save}
       />}
-
+      {mode === SAVING && <Status message={"Saving"}/>}
+      {mode === DELETING && <Status message={"Deleting"}/>}
+      {mode === CONFIRM && (
+        <Confirm
+          onCancel={() => back()}
+          message={"Are you sure you want to delete?"}
+          onConfirm={deleting}
+        />)}
+      {mode === EDIT && (
+        <Form 
+          interviewers={props.interviewers}
+          interviewer={props.interview.interviewer.id}
+          student={props.interview.student}
+          onCancel={() => back(SHOW)}
+          onSave={save}
+        />
+      )}
+      {mode === ERROR_SAVE && (
+        <Error
+          message={"Could not create appointment"}
+          onClose={() => back()}
+        />
+      )}
+      {mode === ERROR_DELETE && (
+        <Error
+          message={"Could not cancel appointment"}
+          onClose={() => back()}
+        />
+      )}
     </article>
 
   );
